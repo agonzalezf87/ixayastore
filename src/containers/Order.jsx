ab0d45d62360aa370d76770531a977154384c560
Antonio Gonzalez <agonzalezf87@gmail.com>
2022-09-10 22:56:23 -0500

    [add] Order component.

--- src/containers/Order.jsx
+++ src/containers/Order.jsx
@@ -1,8 +1,28 @@
+import { calcPercentage, formatEsMX } from '../helpers/numberHandling'
+import moment from 'moment/moment'
+import 'moment/locale/es'
+import _ from 'lodash'
+import '../styles/containers/Order.sass'
+import { ProductInOrder } from '../components/ProductInOrder'
 
-
-const Order = () => {
+const Order = ({data}) => {
   return (
-    <div>Order</div>
+    <div className="Order">
+      <div className="Order__top">
+        <div className="Order__date">{moment(data.lastUpdate).locale('es').format('DD / MM / Y')}</div>
+        <div className="Order__code">Orden: <i>{data.orderCode}</i></div>
+        <div className="Order__status"><span>{parseInt(data.paid) === 1 ? 'Pagado' : 'Pendiente'}</span></div>
+      </div>
+      <div className="Order__address"><b>Dirección de envío:</b> {data.streetName}, {data.address}, {data.city}, {data.state}, {data.zip_code}</div>
+      <div className="Order__subtotal"><b>Sub-total:</b> ${formatEsMX(data.subtotal)}</div>
+      <div className="Order__total"><b>Total:</b> ${formatEsMX(data.total)} <span>-{calcPercentage(data.subtotal, data.discount)}%</span></div>
+      <div className="Order__products">
+        <div><b>Productos:</b></div>
+        {_.uniqBy(data.products, 'id').map(product => (
+          <ProductInOrder imageUrl={product.image_url} title={product.title} price={product.price} qty={product.qty} key={product.id} />
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -2,11 +2,8 @@
 import { useEffect, useState } from 'react'
 import { getOrdersList } from '../helpers/api'
 import { calcPercentage, formatEsMX } from '../helpers/numberHandling'
-import moment from 'moment/moment'
-import 'moment/locale/es'
-import _ from 'lodash'
+import { Order } from '../containers/Order'
 import '../styles/containers/Orders.sass'
-import { ProductInOrder } from '../components/ProductInOrder'
 
 const Orders = () => {
   const [loading, setLoading] = useState(true)
@@ -36,22 +33,7 @@ const Orders = () => {
         {!loading && (
           orders.map(order => (
             order.enabled === '1' && (
-              <div className="Orders__resume" key={order.order_code}>
-                <div className="Orders__resume__top">
-                  <div className="Orders__resume__date">{moment(order.last_update).locale('es').format('DD / MM / Y')}</div>
-                  <div className="Orders__resume__code">Orden: <i>{order.order_code}</i></div>
-                  <div className="Orders__resume__status"><span>{parseInt(order.paid) === 1 ? 'Pagado' : 'Pendiente'}</span></div>
-                </div>
-                <div className="Orders__resume__address"><b>Dirección de envío:</b> {order.street_name}, {order.address}, {order.city}, {order.state}, {order.zip_code}</div>
-                <div className="Orders__resume__subtotal"><b>Sub-total:</b> ${formatEsMX(order.subtotal)}</div>
-                <div className="Orders__resume__total"><b>Total:</b> ${formatEsMX(order.total)} <span>-{calcPercentage(order.subtotal, order.discount)}%</span></div>
-                <div className="Orders__resume__products">
-                  <div><b>Productos:</b></div>
-                  {_.uniqBy(order.products, 'id').map(product => (
-                    <ProductInOrder imageUrl={product.image_url} title={product.title} price={product.price} qty={product.qty} key={product.id} />
-                  ))}
-                </div>
-              </div>
+              <Order data={order} key={order.order_code} />
             )
           ))
         )}
