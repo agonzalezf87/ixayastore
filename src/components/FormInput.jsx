0b4f5dd7b0493a80b1ff5179976b95112bafb1d4
Antonio Gonzalez <agonzalezf87@gmail.com>
2022-09-17 12:04:16 -0500

    [add] Style file for inputs.

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -3,12 +3,12 @@ import '../styles/components/FormInput.sass'
 const FormInput = (props) => {
   return (
     <>
-      <label>
+      <label className='Form__label'>
         {props.label}
         {!props.type ? (
-          <input type="text" name={props.iD} placeholder={props.placeHolder} required />
+          <input className='Form__input' type="text" name={props.iD} placeholder={props.placeHolder} required />
         ) : (
-          <input type={props.type} name={props.iD} placeholder={props.placeHolder} pattern={props.pattern} required />
+          <input className='Form__input' type={props.type} name={props.iD} placeholder={props.placeHolder} pattern={props.pattern} required />
         )}
       </label>
     </>
